1a5b032ed12870545f1d7cdae61a82ef424ace6c
Zbyszek(at)Git <zbigniew.malachowski@gmail.com>
2020-03-09 23:05:14 +0100

    just modified text displayed by app

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          Najnowsza zmiana czyli podła zmiana
+          Nie do wiary, myślałem, że to nie dojdzie do AWS
         </p>
         <a
           className="App-link"
